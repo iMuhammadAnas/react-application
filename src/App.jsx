aabf9e1ba8a4155433f1682ec858ca5052dd7b42
Anas Khan <anasnaseem420@gmail.com>
2022-08-19 06:02:39 +0500

    Adding Bootstrap and Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import './App.css';
 
@@ -17,21 +18,48 @@ class Mainpage extends Component {
 
   render() {
     return (
-      <div className='major'>
+      <div className='major1'>
         {!!this.state.users.length ? this.state.users.map((el) =>
-          <div className='row' key={el.id} >
-            <div className="colum">
-              <div className='card'>
+          <div className='row1' key={el.id} >
+            <div className="colum1">
+              <div className='card1'>
                 <img className='img1' src={el.image} alt="" />
                 <p><b>Name : </b>{el.firstName} {el.lastName}</p>
                 <p><b>Email : </b>{el.email}</p>
                 <p><b>Phone : </b>{el.phone}</p>
                 <p><b>age : </b>{el.age}</p>
                 <p><b>Gender : </b>{el.gender}</p>
-                <button>Show More</button>
+                <button data-bs-toggle="modal" data-bs-target="#staticBackdrop">More About {el.firstName}</button>
               </div>
             </div>
-          </div>) :
+
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="staticBackdropLabel">{el.firstName}</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                    <p><b>Name : </b>{el.firstName}</p>
+                    <p><b>Full Name : </b>{el.firstName} {el.lastName}</p>
+                    <p><b>age : </b>{el.age}</p>
+                    <p><b>Phone : </b>{el.phone}</p>
+                    <p><b>Email : </b>{el.email}</p>
+                    <p><b>Gender : </b>{el.gender}</p>
+                    <p><b>Birth date : </b>{el.birthDate}</p>
+                    <p><b>Address : </b>{el.address.address}</p>
+                    <p><b>City : </b>{el.address.city}</p>
+                  </div>
+                  <div className="modal-footer mf">
+                    <button type="button" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        ) :
           <div className="loading">
             <div className="loader">Loading...</div>
           </div>
